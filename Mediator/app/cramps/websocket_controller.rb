aa8bd11e8894bf9a9f0d75687d32f59fb9af8e9e
Tom Andrews <git@tomandrews.co.uk>
2010-07-14 19:26:37 +0100

    Added useful logging and message counting to ensure it is stateful.

--- Mediator/app/cramps/websocket_controller.rb
+++ Mediator/app/cramps/websocket_controller.rb
@@ -1,19 +1,40 @@
 require 'rubygems'
 require 'cramp/controller'
 require 'thin'
+require 'socket'
 
 class WebsocketController < Cramp::Controller::Websocket
   before_start :test_request
   on_data :received_data
-
+  
+  @count = 0
+  
   def received_data(data)
-  	puts "lol"
-    render "Got your #{data}"
+    increment 
+  	puts "Received: #{@count.to_s} #{data}"
+    render "Got your #{@count.to_s} #{data}"
   end
   
   def test_request
-    puts "Going to reject them"
-    halt 500, {'Content-Type' => 'text/plain'}, "Invalid ID"
+    puts "Web socket connection requested"
+    yield
+    #halt 500, {'Content-Type' => 'text/plain'}, "Invalid ID"
+  end
+  
+  private
+  def create_connection
+    host = '192.168.1.65'
+    port = '41588'
+    @conn = UDPSocket.new
+    @conn.connect(host, port)
+  end
+  
+  def increment
+    if @count.nil?
+      @count = 0
+    else
+      @count += 1
+    end
   end
 end
 
